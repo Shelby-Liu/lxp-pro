e7bbdad5cc70c5b66eaadfa8048ef528253433a8
PlutoSteven <liu19950788499@163.com>
2019-12-11 21:10:10 +0800

    Merge branch 'master' of D:\workspace\lxp-pro with conflicts.

--- lxp-pro-front/src/views/my-project/Login.vue
+++ lxp-pro-front/src/views/my-project/Login.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    <el-input v-model="loginForm.username" placeholder="请输入用户名" />
-    <br><br>
-    <el-input v-model="loginForm.password" type="password" placeholder="请输入密码" />
-    <br><br>
-    <el-button type="primary">登录</el-button>
-  </div>
+  <body id="form">
+    <el-form class="login_form">
+      <el-form-item>
+        <h3 class="login_title">欢迎登陆</h3>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="loginForm.username" type="text" placeholder="用户名" />
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="loginForm.password" type="password" placeholder="密码" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" size="medium" @click="login">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </body>
 </template>
 
 <script>
@@ -16,8 +25,9 @@ export default {
   data() {
     return {
       loginForm: {
-        username: '',
-        password: ''
+        username: 'admin',
+        password: '123456'
+
       },
       responseResult: []
     }
@@ -41,3 +51,32 @@ export default {
 }
 </script>
 
+<style>
+  body{
+    margin: 0px;
+  }
+  #form{
+    background-image: url("../../../public/login_bg.jpg");
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  .login_form{
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 200px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+
+  .login_title {
+    margin: 0px auto 10px auto;
+    text-align: center;
+    color: #505458;
+  }
+</style>
